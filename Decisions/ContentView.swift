0fc76fee727cd4edeeb7422c8ebeba884e934f1e
besarismaili <b3sar11@gmail.com>
2019-09-25 19:25:39 +0200

    initial push

---
+++ Decisions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Decisions
+//
+//  Created by Besar Ismaili on 25/09/2019.
+//  Copyright © 2019 Besar Ismaili. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
